cb31b6d6c1233964890852d8ce49016640e1e602
Nate Brake <njbrake@gmail.com>
2018-08-11 18:21:48 -0400

    login authactions

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,49 @@
-import { TEST_DISPATCH } from "./types";
-
+import axios from "axios";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 //Register user
-export const registerUser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+export const registerUser = (userData, history) => dispatch => {
+	axios
+		.post("/api/users/register", userData)
+		.then(res => history.push("/login"))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+//Login user
+export const loginUser = userData => dispatch => {
+	axios
+		.post("/api/users/login", userData)
+		.then(res => {
+			//save to local storage
+			const { token } = res.data;
+			//set token to local storage
+			localStorage.setItem("jwtToken", token);
+			//Set the token to be the auth header
+			setAuthToken(token);
+			//Decode token to get user userData
+			const decoded = jwt_decode(token);
+			//Set current user
+			dispatch(setCurrentUser(decoded));
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+//Set logged in user
+
+export const setCurrentUser = decoded => {
+	return {
+		type: SET_CURRENT_USER,
+		payload: decoded
+	};
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,1 +1,2 @@
-export const TEST_DISPATCH = "test dispatch";
+export const GET_ERRORS = "GET_ERRORS";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
