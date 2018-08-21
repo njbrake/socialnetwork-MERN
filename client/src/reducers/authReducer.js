1fb9a687d3139352c9a0dc2a106a1b64329a1a65
Nate <njbrake@gmail.com>
2018-08-20 20:01:07 -0400

    Login

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,24 +2,24 @@ import isEmpty from "../validation/is-empty";
 import { GET_ERRORS, SET_CURRENT_USER } from "../actions/types";
 
 const initialState = {
-	isAuthenticated: false,
-	user: {}
+  isAuthenticated: false,
+  user: {}
 };
 
 export default function(state = initialState, action) {
-	switch (action.type) {
-		case GET_ERRORS:
-			return {
-				...state,
-				user: action.payload
-			};
-		case SET_CURRENT_USER:
-			return {
-				...state,
-				isAuthenticated: !isEmpty(action.payload),
-				user: action.payload
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case GET_ERRORS:
+      return {
+        ...state,
+        user: action.payload
+      };
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
+    default:
+      return state;
+  }
 }
