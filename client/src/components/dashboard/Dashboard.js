5cefe2b054e68f17b95d11a76cd072fa07b9280a
Nate <njbrake@gmail.com>
2018-11-27 21:17:50 -0500

    last air

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions';
 
 import { getCurrentProfile } from '../../actions/profileActions';
-import TextFieldGroup from '../common/TextFieldGroup';
+import Spinner from '../common/Spinner.js';
 
 class Dashboard extends Component {
 	componentDidMount() {
 		this.props.getCurrentProfile();
 	}
 	render() {
-		return <h1> Dashboard</h1>;
+		const { user } = this.props.auth;
+		const { profile, loading } = this.props.profile;
+		let dashboardContent;
+		if (profile === null || loading === true) {
+			dashboardContent = <Spinner />
+		} else {
+			//check if user has profile data
+			if (Object.keys(profile).length > 0) {
+				dashboardContent = <h4>DISPLAY PROFILE </h4>
+			} else {
+				//User is logged in but no profile
+				dashboardContent = <div>
+					<p className="lead text-muted">
+						Welcome {user.name}
+					</p>
+					<p className="lead"> You have not yet set up your profile please add some info </p>
+					<Link to='/create-profile' className="btn btn-lg btn-info"> Create Profile </Link>
+				</div>
+			}
+		}
+		return (
+			<div className='dashboard'>
+				<div className='container'>
+					<div className='row' >
+						<div clssName="col-md-12">
+							<div className="display-4"> {dashboardContent}</div>
+						</div></div>
+				</div>
+			</div>
+		)
+
 	}
 }
 
+Dashboard.PropTypes = {
+	getCurrentProfile: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired
+}
 const mapStateToProps = state => ({
 	auth: state.auth,
 	errors: state.errors,
+	profile: state.profile
 });
 export default connect(
 	mapStateToProps,
 	{ getCurrentProfile },
-)(withRouter(Dashboard));
+)(Dashboard);
